b823ebea0207817662f797b7699c4c351bd5e614
arunkoffeekode <115154631+arunkoffeekode@users.noreply.github.com>
2024-10-01 16:47:12 +0530

    chane

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#1A1B2F', // Dark background
+        teal: {
+          300: '#34D399', // For headings
+        },
+        red: {
+          400: '#EF4444', // For subheadings
+        },
+      },
+    },
   },
   plugins: [],
 }
